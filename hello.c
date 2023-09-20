b2aa40e5854fe1f342e4e4599bc9afa45b1da881
oscartangozero <47040135+oscartangozero@users.noreply.github.com>
2023-09-20 16:59:56 +0000

    20.09

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Hello, world!");
+    // int(input()) + input()
+    // scanf("%i", &x); // i == int, d == decimal
+    // scanf("%lf", &x); // lf == long float == double
+    printf("%lf", cos(1.0/2.0));
     return 0;
 }
