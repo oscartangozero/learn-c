6d2408bcc37d72b56e815ad07d1b8c6ccf47ea3f
oscartangozero <47040135+oscartangozero@users.noreply.github.com>
2023-09-22 20:07:43 +0000

    class 1: refactor

--- 1-intro/hello.c
+++ 1-intro/hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // для доступа к cos()
 
 int main() {
     int x;
-    // scanf("%i", &x); // i == int, d == decimal
+    // scanf("%i", &x); // i == int == d == decimal
     // scanf("%lf", &x); // lf == long float == double
     x = cos(1.0/2.0);
     printf("%d", x);
